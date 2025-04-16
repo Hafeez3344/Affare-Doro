c3ada16e7c779c56d8b1668582604886c925c692
Hafeez Ur Rahman <khan3344kpk@gmail.com>
2025-04-16 19:21:45 +0500

    set admin login

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -17,22 +17,31 @@ import {
 } from "react-icons/hi2";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 import { updateSidebar } from "@/features/features";
-
+import { LuLogOut } from "react-icons/lu";
 
 const Sidebar = ({ showModal }) => {
   const navigate = useRouter();
   const dispatch = useDispatch();
   const showSidebar = useSelector((state) => state.showSidebar);
+  
   const fn_sidebarControl = () => {
     dispatch(updateSidebar(!showSidebar));
   };
+
+  const handleLogout = () => {
+    // Clear any stored tokens or user data here
+    localStorage.clear();
+    // Redirect to login page
+    navigate.push('/login');
+  };
+
   return (
     <>
       <div
         className={`${showSidebar ? "absolute md:relative flex h-[85%] min-h-[550px] md:h-auto" : "absolute md:relative hidden md:flex md:h-auto"
           } w-[215px] bg-white rounded-tr-[8px] mt-[30px] shadow-2xl md:shadow-md px-[10px] py-[20px] flex-col gap-0.5 z-[9] ${
           showModal ? "opacity-50 pointer-events-none" : "opacity-100"
-        }`}
+        } relative`}
       >
         <button
           className="absolute md:hidden text-[var(--text-color)] right-5 scale-[1.5] top-2"
@@ -140,6 +149,15 @@ const Sidebar = ({ showModal }) => {
           navigateTo={"help-store"}
           navigate={navigate}
         /> */}
+        <div 
+          onClick={handleLogout}
+          className={`flex h-[48px] items-center gap-3 px-[10px] hover:text-[var(--text-color)] cursor-pointer hover:bg-[var(--bg-color)] border-l-[2px] hover:border-[var(--text-color)] w-full text-gray-500 bg-transparent border-white absolute bottom-0`}
+        >
+          <div className="flex-shrink-0">
+            <LuLogOut className="w-[20px] h-[20px] rotate-180" />
+          </div>
+          <p className="text-[13px] font-[500] whitespace-nowrap">Logout</p>
+        </div>
       </div>
       <button
         className={`${showSidebar ? "hidden" : "absolute"} md:hidden text-[var(--text-color)] left-5 scale-[1.5] top-[77px]`}
