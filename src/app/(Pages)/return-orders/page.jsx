527cda2d29133f8e95fdf878b4ce4a1518c6d874
erbazzafar <erbazzafar11@gmail.com>
2025-10-18 15:35:05 +0500

    updated delivery status condition

--- src/app/(Pages)/return-orders/page.jsx
+++ src/app/(Pages)/return-orders/page.jsx
@@ -208,7 +208,7 @@ const ReturnOrders = () => {
                                                     </div>
                                                 </td>
                                                 <td className="p-4 capitalize">
-                                                    {order.status || "Unknown"}
+                                                    {order?.deliveryId?.status || "Unknown"}
                                                 </td>
                                                 <td className="p-4">
                                                     <span className={`px-3 py-1 rounded-full text-xs font-semibold capitalize ${order.paidByAdmin === "paid"
@@ -230,7 +230,7 @@ const ReturnOrders = () => {
                                                         >
                                                             <FiEye />
                                                         </button>
-                                                        {order.status?.toLowerCase() === "delivered" && (
+                                                        {order?.deliveryId?.status?.toLowerCase() === "delivered" && (
                                                             <button
                                                                 className="bg-green-100 text-green-600 rounded-full px-2 py-2"
                                                                 title="Payment Details"
